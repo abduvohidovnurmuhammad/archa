0307d783535f34adf168ef2d2c26971d2213efb7
abduvohidovnurmuhammad <abduvohidovn7@gmail.com>
2022-11-09 20:53:25 +0500

    archa

---
+++ main.js
@@ -0,0 +1,15 @@
+let kirish = prompt( "hush kelibsiz")
+console.log("hush kelibsiz:" + kirish)
+let ism = prompt("Ismingiz")
+console.log("Ismingiz" + ism)
+let yosh = prompt("yoshingiz")
+console.log("yoshingiz" + yosh)
+let sayt = prompt("saytimiz yoqdimi?")
+console.log ("saytimiz" + sayt)
+console.log('      🌟 ');
+console.log('   ⭐⭐⭐');
+console.log('  ⭐⭐⭐⭐');
+console.log(' ⭐⭐⭐⭐⭐');
+console.log('⭐⭐⭐⭐⭐⭐');
+console.log('     📍 📍');
+console.log('     📍 📍');
